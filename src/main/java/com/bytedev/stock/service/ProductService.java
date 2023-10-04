87e78748737fab71bfbe4a527458bc221bcc18ec
nicolas-limadev <nicolasbasto76@gmail.com>
2023-10-04 00:07:25 -0300

    adicionado repository nos services

--- src/main/java/com/bytedev/stock/service/ProductService.java
+++ src/main/java/com/bytedev/stock/service/ProductService.java
@@ -1,6 +1,11 @@
 package com.bytedev.stock.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bytedev.stock.repository.ProductRepository;
+
 public class ProductService {
     
-    
+    @Autowired
+    private ProductRepository productRepository;
 }
--- src/main/java/com/bytedev/stock/service/StockService.java
+++ src/main/java/com/bytedev/stock/service/StockService.java
@@ -1,5 +1,8 @@
 package com.bytedev.stock.service;
 
+import com.bytedev.stock.repository.StockRepository;
+
 public class StockService {
     
+    private StockRepository stockRepository;
 }
