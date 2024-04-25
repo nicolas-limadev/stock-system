7474f526dc3e7cf7358239062322ec91af708de0
nicolas-limadev <nicolasbasto76@gmail.com>
2024-04-24 22:33:35 -0300

    fix: remove imports not used

--- src/main/java/com/bytedev/storage/controller/ProductController.java
+++ src/main/java/com/bytedev/storage/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.bytedev.storage.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
