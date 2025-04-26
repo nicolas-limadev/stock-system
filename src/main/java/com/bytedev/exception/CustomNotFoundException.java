42eaf79e93f178bfd01478fa0eb54a5df7275618
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-04-26 20:58:23 -0300

    Adicionar CustomNotFoundException.java

---
+++ src/main/java/com/bytedev/exception/CustomNotFoundException.java
@@ -0,0 +1,7 @@
+package main.java.com.bytedev.exception;
+
+public class CustomNotFoundException extends RuntimeException {
+    public CustomNotFoundException(String message) {
+        super(message);
+    }
+}
